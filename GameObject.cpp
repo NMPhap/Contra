047200b4a799a63d43c86720cd40607fd04b593f
NMPhap <116002697+NMPhap@users.noreply.github.com>
2023-04-05 14:39:15 +0700

    <Fix>: Reup CGameObject

--- GameObject.cpp
+++ GameObject.cpp
@@ -1,1 +1,24 @@
+#include <d3dx9.h>
+
+#include "debug.h"
+#include "Game.h"
 #include "GameObject.h"
+CGameObject::CGameObject()
+{
+	x = y = 0;
+	vx = vy = 0;
+	nx = 1;	
+	state = -1;
+}
+
+void CGameObject::DeleteFromGameObjects()
+{
+	CGame* game = CGame::GetInstance();
+	vector<LPGAMEOBJECT> gameObjects = CGame::GetInstance()->gameObjects;
+	vector<LPGAMEOBJECT>::iterator i = std::find(gameObjects.begin(), gameObjects.end(), this);
+	gameObjects.erase(i);
+}
+
+CGameObject::~CGameObject()
+{
+}
--- GameObject.h
+++ GameObject.h
@@ -3,33 +3,37 @@
 #include <Windows.h>
 #include <d3dx10.h>
 #include <vector>
-
+#include "Animations.h"
 using namespace std;
 
+class CGame;
 class CGameObject
 {
 protected:
-	float x;
+	float x; 
 	float y;
 
 	float vx;
 	float vy;
 
-	int nx;
-
-	int state;
-public:
+	int nx;	 
+	int state;									
+public: 
 	void SetPosition(float x, float y) { this->x = x, this->y = y; }
-	void SetSpeed(float vx, float vy) { this->vx = vx, this->vy = vy; }
-
+	void SetSpeedX(float vx) { this->vx = vx; }
+	void SetSpeedY(float vy) { this->vy = vy; }
 	void SetState(int state) { this->state = state; }
 	int GetState() { return this->state; }
 
 	CGameObject();
-	CGameObject(float x, float y) :CGameObject() { this->x = x; this->y = y; }
+	CGameObject(float x, float y) :CGameObject() { this->x = x; this->y = y; this->vx = 0; this->vy = 0; }
 
 	virtual void Update(DWORD dt) = 0;
+	virtual void HandleInput() {};
 	virtual void Render() = 0;
+	virtual void DeleteFromGameObjects();
+	float GetX() { return x; }
+	float GetY() { return y; }
 	~CGameObject();
 };
 
